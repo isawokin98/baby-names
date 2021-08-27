540c07c22dd4678504ef32d1366f3b7514cedf0b
ibrahimabdu <ibrahim.abdu777@gmail.com>
2021-08-27 09:52:48 +0100

    fixed types

--- src/components/FavouritesList.tsx
+++ src/components/FavouritesList.tsx
@@ -1,14 +1,25 @@
-import './buttons.css'
-import {Name} from './MainBody'
+import "./buttons.css";
+import { Name } from "./MainBody";
 
 interface FavouritesListProps {
-    favourites: Name[] ;
-    handleFavouriteClick: any;
+  favourites: Name[];
+  handleFavouriteClick: React.MouseEventHandler<HTMLButtonElement>;
 }
-export default function FavouritesList({favourites, handleFavouriteClick}:FavouritesListProps):JSX.Element {
-    return <>
-   {favourites.map((favourite,idx) => <button key ={idx} onClick={handleFavouriteClick} className={favourite.sex === 'f'? 'buttonFemale': 'buttonMale'}>{favourite.name}</button>)} 
+export default function FavouritesList({
+  favourites,
+  handleFavouriteClick,
+}: FavouritesListProps): JSX.Element {
+  return (
+    <>
+      {favourites.map((favourite, idx) => (
+        <button
+          key={idx}
+          onClick={handleFavouriteClick}
+          className={favourite.sex === "f" ? "buttonFemale" : "buttonMale"}
+        >
+          {favourite.name}
+        </button>
+      ))}
     </>
+  );
 }
-
-{/* <div>{favourites.join(' ')}</div> */}
--- src/components/NamesList.tsx
+++ src/components/NamesList.tsx
@@ -1,30 +1,40 @@
-import data from '../data.json'
-import { Name } from './MainBody'
-import NameItem from './NameItem'
+import { Name } from "./MainBody";
+import NameItem from "./NameItem";
 
 interface NamesListProps {
-    search: string;
-    //click: string;
-    handleClickName: any;
-    favourites: Name[];
-    genderedData: Name[];
-
+  search: string;
+  handleClickName: React.MouseEventHandler<HTMLButtonElement>;
+  favourites: Name[];
+  genderedData: Name[];
 }
-//click
-export default function NamesList({search,  handleClickName, favourites, genderedData}: NamesListProps):JSX.Element {
-    
-    genderedData.sort(function(a: Name,b: Name){
-        if(a.name < b.name) return -1
-        if (a.name > b.name) return 1
-        return 0
-    })
+export default function NamesList({
+  search,
+  handleClickName,
+  favourites,
+  genderedData,
+}: NamesListProps): JSX.Element {
+  genderedData.sort(function (a: Name, b: Name) {
+    if (a.name < b.name) return -1;
+    if (a.name > b.name) return 1;
+    return 0;
+  });
 
-    const filteredData = genderedData.filter(item => item.name.toLowerCase().includes(search.toLowerCase()) && !favourites.includes(item))
-    
-    
-    return <>
-{filteredData.map(item => <NameItem key={item.id} name={item.name} sex={item.sex} handleClickName={handleClickName}/>)}
+  const filteredData = genderedData.filter(
+    (item) =>
+      item.name.toLowerCase().includes(search.toLowerCase()) &&
+      !favourites.includes(item)
+  );
 
+  return (
+    <>
+      {filteredData.map((item) => (
+        <NameItem
+          key={item.id}
+          name={item.name}
+          sex={item.sex}
+          handleClickName={handleClickName}
+        />
+      ))}
     </>
+  );
 }
-//click={click} 
